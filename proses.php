2b79c808e7b803ca1c4d4348fd227c4627c14968
Ugiy <43519843+Lugiy@users.noreply.github.com>
2018-09-28 10:08:15 +0700

    Proses
    Proses

--- proses.php
+++ proses.php
@@ -1,4 +1,25 @@
-<?php
+<?php 
+
+  if (isset($_POST['kirim'])) {
+    $username=$_POST=['username']
+    $password=$_POST=['password']
+    $user=array(
+                "user"=>"lugi",
+                "pass"=>"yunus");
+
+    if($username==$user['user']&&$password==$user['pass']) {
+      echo "oke"."<br>";
+    }
 
 
 ?>
+    
+    <form method="POST">
+      Makanan  <input type="checkbox" name='makanan[]' value="Rujak"></td>
+      buah  <input type="checkbox" name='buah[]' value="Mangga"></td>
+      Laptop  <input type="checkbox" name='laptop[]' value="MSI"></td>
+      <input type="submit" name="submit" value="login">
+    </form>>
+
+  }
+ ?>
